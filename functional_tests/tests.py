dabebb2adc45495090792a84ede9e545e7af2505
wangxiaolong <wangxl@winside.cn>
2019-03-29 18:27:06 +0800

    1. 增加重试次数，显性等待比selenium的隐式等待好 2. 让每个用户都能保存自己的清单

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,25 +1,34 @@
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
-
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# 伊迪丝听说有一个很酷的在线待办事项应用
 		# 她去看了这个应用的首页
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		# 她注意到网页的标题和头部都包含“To-Do”这个词
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -38,8 +47,7 @@ class NewVisitorTest(unittest.TestCase):
 		# 她按回车键后，页面更新了
 		# 待办事项表格中显示了“1: Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		# 页面中又显示了一个文本框，可以输入其他的待办事项
 		# 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
@@ -47,12 +55,10 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
 
 		# 页面再次更新，她的清单中显示了这两个待办事项
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# 伊迪丝想知道这个网站是否会记住她的清单
 		# 她看到网站为她生成了一个唯一的URL
@@ -63,10 +69,50 @@ class NewVisitorTest(unittest.TestCase):
 		# 她很满意，去睡觉了
 		self.fail('Finish the test!')
 
+	def test_can_start_a_list_for_one_user(self):
+		# 页面再次更新，她的清单中显示了这两个待办事项
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# 伊迪丝新建一个待办事项清单
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		# 她注意到清单有个唯一的URL
+		edit_list_url = self.browser.current_url
+		self.assertRegex(edit_list_url, '/lists/.+')
+		# 现在一名叫作弗朗西斯的新用户访问了网站
 
+		## 我们使用一个新浏览器会话
+		## 确保伊迪丝的信息不会从cookie中泄露出去
+		self.browser.quit()
+		self.browser = webdriver.Chrome()
 
+		# 弗朗西斯访问首页
+		# 页面中看不到伊迪丝的清单
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
 
+		# 弗朗西斯输入一个新待办事项，新建一个清单
+		# 他不像伊迪丝那样兴趣盎然
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
 
+		# 弗朗西斯获得了他的唯一URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edit_list_url)
+
+		# 这个页面还是没有伊迪丝的清单
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		# 两人都很满意，然后去睡觉了
